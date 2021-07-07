aa50cdcc073fc75f6ade38f8d98f7e3767a7247d
asiful086 <asifulhaque086@gmail.com>
2021-07-07 12:59:41 +0600

    back to home button linked in 404 page

--- src/pages/Four0Four.js
+++ src/pages/Four0Four.js
@@ -1,18 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Four0Four = () => {
   return (
     <>
       <section className="four-zero-four">
-        <img className="bg-img" src="assets/images/404-bg.png" alt />
+        <img className="bg-img" src="assets/images/404-bg.png" alt="random" />
         <div className="container">
           <div className="row">
             <div className="col-lg-12">
               <div className="content">
-                <img src="assets/images/404.png" alt />
+                <img src="assets/images/404.png" alt="random" />
                 <div className="inner-content">
                   <h4 className="title">Oops, Something went wrong !</h4>
-                  <a href="index.html" className="mybtn1">
+                  <Link to="/" className="mybtn1">
                     <i className="fas fa-angle-double-left" /> BACK TO HOME
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
